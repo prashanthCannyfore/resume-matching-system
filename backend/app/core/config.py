cdc8471915516462fcf3ac6ad586f7559f480fc1
abiramigramener <abirami.v@gramenerit.com>
2026-05-05 15:25:54 +0530

    Feat:Cors issue fixed

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -18,11 +18,10 @@ class Settings(BaseSettings):
 
     # CORS
     ALLOWED_ORIGINS: List[str] = [
-        "http://localhost:5173",  # Vue default port (Vite)
+        "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "*",
     ]
 
     # ================== DATABASE ==================
